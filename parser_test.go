946fe20af9ea9a602fe1d967ea5e4f1bbaddea19
Simon Tucker <brotherlogic@gmail.com>
2016-12-28 14:34:24 -0800

    Enabled basic evaluation. This closes #2.

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,9 @@
 package golisp
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestIntegerRep(t *testing.T) {
 	v := &Integer{value: 5}
@@ -11,7 +14,25 @@ func TestIntegerRep(t *testing.T) {
 
 func TestListRep(t *testing.T) {
 	v := &List{start: &listNode{value: &Integer{value: 5}, next: &listNode{value: &Integer{value: 10}}}}
-	if v.str() != "( 5 10 )" {
+	if v.str() != "(5 10)" {
 		t.Errorf("List str method fails: %v from %v", v.str(), v)
+	}
+}
+
+var parsetestdata = []struct {
+	expression string
+	parsed     Primitive
+}{
+	{"5", Integer{value: 5}},
+	{"(1 2 3)", List{start: &listNode{value: Integer{value: 1}, next: &listNode{value: Integer{value: 2}, next: &listNode{value: Integer{value: 3}}}}}},
+	{"(+ 2 3)", List{start: &listNode{value: Operator{value: "+"}, next: &listNode{value: Integer{value: 2}, next: &listNode{value: Integer{value: 3}}}}}},
+}
+
+func TestParse(t *testing.T) {
+	for _, test := range parsetestdata {
+		result := Parse(test.expression)
+		if !reflect.DeepEqual(test.parsed, result) {
+			t.Errorf("Parse fail %v->%v (should be %v)", test.expression, result.str(), test.parsed.str())
+		}
 	}
 }
