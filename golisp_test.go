8a04998f3fd92d11feaf84830211ed52961fb671
Simon Tucker <brotherlogic@gmail.com>
2017-01-28 08:38:38 -0800

    Added test for number 35. This closes #61.

--- golisp_test.go
+++ golisp_test.go
@@ -34,6 +34,7 @@ var testdata = []struct {
 	{[]string{"(list 'buy (* 27 34) 'bagels)"}, []string{"(buy 918 bagels)"}},
 	{[]string{"'(foo bar baz)"}, []string{"(foo bar baz)"}},
 	{[]string{"(list 'foo 'bar 'baz)"}, []string{"(foo bar baz)"}},
+	{[]string{"(cons 'foo '(bar baz))"}, []string{"(foo bar baz)"}},
 }
 
 var baddata = []struct {
