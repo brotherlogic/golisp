e44d0223202f02b5c20efebd7e38aeeb1e98e196
Simon Tucker <brotherlogic@gmail.com>
2022-08-22 17:40:28 -0700

    Step0 (#260)

--- list_test.go
+++ list_test.go
@@ -1,16 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
 	"testing"
 )
-
-
-func TestBasic(t *testing.T) {
-	t.Errorf("Not ready for testing")
-}
 
 func Test0(t *testing.T) {
 	lines, err := os.ReadFile("testdata/test0.txt")
@@ -18,9 +14,16 @@ func Test0(t *testing.T) {
 		t.Fatalf("Cannot read file: %v", err)
 	}
 
+	buffer := ""
 	for _, line := range strings.Split(string(lines), "\n") {
 		if strings.HasPrefix(line, ";;") {
 			log.Printf("%v", line)
+		} else if strings.HasPrefix(line, ";") {
+			if buffer != line[1:] {
+				t.Errorf("Bad Process: %v vs %v", buffer, line)
+			}
+		} else if len(line) > 0 {
+			buffer = fmt.Sprintf("=>%v", rep(line))
 		}
 	}
 }
