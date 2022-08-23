87520a0b5180630529fa5557ecf9a028f867d841
Simon Tucker <brotherlogic@gmail.com>
2022-08-22 17:23:18 -0700

    Step0 (#259)

--- list_test.go
+++ list_test.go
@@ -1,7 +1,26 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
 
 func TestBasic(t *testing.T) {
 	t.Errorf("Not ready for testing")
+}
+
+func Test0(t *testing.T) {
+	lines, err := os.ReadFile("testdata/test0.txt")
+	if err != nil {
+		t.Fatalf("Cannot read file: %v", err)
+	}
+
+	for _, line := range strings.Split(string(lines), "\n") {
+		if strings.HasPrefix(line, ";;") {
+			log.Printf("%v", line)
+		}
+	}
 }
